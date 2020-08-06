df35d81d438424283b0b49a1335d64dbc7e6b04a
Felipe Nascimento <felipe.rj@hotmail.com.br>
2020-08-05 21:06:52 -0300

    feat: finish screen home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
-export default function Home () {
-  return null;
+import {
+  MaterialCommunityIcons,
+  AntDesign
+} from '@expo/vector-icons';
+
+import {
+  Wrapper,
+  Container,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance
+} from './styles';
+
+import Suggestions from '../../components/suggestions';
+import Activities from '../../components/activities';
+import Tips from '../../components/tips';
+import Banner from '../../components/banner';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 509,87</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+        <Activities />
+        <Tips />
+        <Banner />
+      </Container>
+    </Wrapper>
+  );
 }
